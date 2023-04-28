416574ad4470c754b7be71df66bb34c9fb93df35
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-27 21:37:25 -0300

    changing fucntion setCount to arrow functions

--- src/App.js
+++ src/App.js
@@ -6,24 +6,20 @@ function App() {
   const  [count, setcount] = React.useState(0);
   
 
-  function handleClickUp(){
+  function add(){
    
-    // setcount(function(){
-    //   return count +1;
+    setcount(prevCount => prevCount + 1)
+    // setcount(function(oldValue){
+    //   return oldValue +1;
     // })
-    setcount(function(oldValue){
-      return oldValue +1;
-    })
     
   }
-  function handleClickDown(){
+  function subtract(){
     
-    // setcount(function(){
-    //   return count - 1;
+    setcount(prevCount => prevCount - 1)
+    // setcount(function(oldValue){
+    //   return oldValue - 1;
     // })
-    setcount(function(oldValue){
-      return oldValue - 1;
-    })
     
     
   }
@@ -32,13 +28,13 @@ function App() {
       
       <div className='number-container'>
         <button 
-         onClick={handleClickDown}
+         onClick={subtract}
         
         className='pminus'>-</button>
         <button
          className='number-content'>{count}</button>
          <button 
-         onClick={handleClickUp}
+         onClick={add}
          className='pplus'>+</button>
       </div>
       
